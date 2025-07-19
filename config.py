202c300cccba31573b9d9e3237cd19d3f7e23f82
ARYAN x SHOAIB <shoaib910385@gmail.com>
2025-07-19 15:26:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/HKMUSIC/YORSAMUSICBOT",
+    "https://github.com/HKMUSIC/yormusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
